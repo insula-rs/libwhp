a796bae35f36f3371dfc4f74e83c61793f04c5e9
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2018-07-09 21:34:04 +0300

    Fix UINT64 type definition

--- src/common.rs
+++ src/common.rs
@@ -16,12 +16,12 @@
 
 use std::error::Error;
 use std::fmt;
-use std::os::raw::{c_int, c_longlong, c_uchar, c_uint, c_ushort, c_void};
+use std::os::raw::{c_int, c_uchar, c_uint, c_ulonglong, c_ushort, c_void};
 
 pub type HRESULT = c_int;
 pub type UINT8 = c_uchar;
 pub type UINT32 = c_uint;
-pub type UINT64 = c_longlong;
+pub type UINT64 = c_ulonglong;
 pub type VOID = c_void;
 pub type BOOL = c_int;
 pub type UINT16 = c_ushort;
